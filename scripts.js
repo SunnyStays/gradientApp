017b9ce0b00cdff484ec4f4bfd2e901d2c0bc2fc
SunnyStays <107935126+SunnyStays@users.noreply.github.com>
2022-08-22 16:05:34 +0200

    display color value

--- scripts.js
+++ scripts.js
@@ -1,60 +1,72 @@
+function colorChannelMix(colorChannelA, colorChannelB, mixValue){
+    let channelA = colorChannelA*mixValue
+    let channelB = colorChannelB*(1-mixValue)
 
-            function colorChannelMix(colorChannelA, colorChannelB, mixValue){
-                let channelA = colorChannelA*mixValue
-                let channelB = colorChannelB*(1-mixValue)
+    return parseInt(channelA+channelB)
+}
 
-                return parseInt(channelA+channelB)
-            }
+function colorMix(rgbA, rgbB, mixValue){
+    let r = colorChannelMix(rgbA[0],rgbB[0],mixValue)
+    let g = colorChannelMix(rgbA[1],rgbB[1],mixValue)
+    let b = colorChannelMix(rgbA[2],rgbB[2],mixValue)
 
-            function colorMix(rgbA, rgbB, mixValue){
-                let r = colorChannelMix(rgbA[0],rgbB[0],mixValue)
-                let g = colorChannelMix(rgbA[1],rgbB[1],mixValue)
-                let b = colorChannelMix(rgbA[2],rgbB[2],mixValue)
+    return "rgb("+r+","+g+","+b+")"
+}
 
-                return "rgb("+r+","+g+","+b+")"
-            }
+function returnColor(color) {
+    const r = parseInt(color.substr(1,2), 16)
+    const g = parseInt(color.substr(3,2), 16)
+    const b = parseInt(color.substr(5,2), 16)
+    return [r,g,b]
+}
 
-            function returnColor(color) {
-                const r = parseInt(color.substr(1,2), 16)
-                const g = parseInt(color.substr(3,2), 16)
-                const b = parseInt(color.substr(5,2), 16)
-                return [r,g,b]
-            }
+function generateGradient(){
+    var colorOne = $("#colorOne").val()
+    var colorTwo = $("#colorTwo").val()
 
-            function generateGradient(){
-                var colorOne = $("#colorOne").val()
-                var colorTwo = $("#colorTwo").val()
+    var childrenCount = document.getElementById("gradient-container").childElementCount
 
-                var childrenCount = document.getElementById("gradient-container").childElementCount
+    for (let i = 0; i<=childrenCount;i++){
 
-                for (let i = 0; i<=childrenCount;i++){
+        let gradientId = "#gradient"+i
+        let mixValue = 1.0/childrenCount*i
+        let backgroundColor = colorMix(returnColor(colorOne),returnColor(colorTwo),mixValue)
 
-                    let mixValue = 1.0/childrenCount*i
+        $(gradientId).css("background-color",backgroundColor)
 
-                    $("#gradient"+i).css("background-color",colorMix(returnColor(colorOne),returnColor(colorTwo),mixValue))
-                }
-            }
+        
+    }
+}
 
-            function addGradientBox(){
-                let childrenCount = document.getElementById("gradient-container").childElementCount
-                if (childrenCount<64){
-                    let gradientBox = "<div class='gradient-box' id='gradient"+parseInt(childrenCount+1)+"' ></div>"
+function addGradientBox(){
+    let childrenCount = document.getElementById("gradient-container").childElementCount
+    if (childrenCount<64){
+        let gradientBox = "<div class='gradient-box' id='gradient"+parseInt(childrenCount+1)+"' ></div>"
 
-                    $("#gradient-container").append(gradientBox)
+        $("#gradient-container").append(gradientBox)
 
-                    generateGradient()
-                }
-            }
+        generateGradient()
+    }
+}
 
-            function removeGradientBox(){
-                if (document.getElementById("gradient-container").childElementCount>1){
-                    $('#gradient-container').children().last().remove();
-                }
+function removeGradientBox(){
+    if (document.getElementById("gradient-container").childElementCount>1){
+        $('#gradient-container').children().last().remove();
+    }
 
-                generateGradient()
-            }
+    generateGradient()
+}
 
-            generateGradient()
 
-            colorOne.addEventListener("change", generateGradient, false);
-            colorTwo.addEventListener("change", generateGradient, false);
+const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
+
+generateGradient()
+
+$(document).on("click",'.gradient-box',function() {  
+    //alert(rgb2hex($(this).css("background-color")))
+    //alert(this.id)
+    $("#color-text").text("This color is "+rgb2hex($(this).css("background-color"))).css("background-color",$(this).css("background-color"))
+});
+
+colorOne.addEventListener("change", generateGradient, false);
+colorTwo.addEventListener("change", generateGradient, false);
